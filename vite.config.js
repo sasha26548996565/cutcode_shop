a97a065c092175e722142e5d80e3aba508850fcf
Sasha <sashapozhidaev07@gmail.com>
2023-10-01 19:58:15 +0000

    confirm order with send email

--- vite.config.js
+++ vite.config.js
@@ -8,7 +8,6 @@ export default defineConfig({
                 'resources/css/app.css',
                 'resources/sass/main.sass',
                 'resources/js/app.js',
-                'resources/js/sendPromocode.js',
             ],
             refresh: true,
         }),
